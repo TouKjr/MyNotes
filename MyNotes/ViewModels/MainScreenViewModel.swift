55919cfed97b3f5eb5115bfdd29f35274a18cc10
Denis Priiskalov <127773745+TouKjr@users.noreply.github.com>
2024-02-01 17:21:12 +0300

    Tasks now saves + example note for the first launch (#2)
    Co-authored-by: Toka <rempriiskalova@gmail.com>

--- MyNotes/ViewModels/MainScreenViewModel.swift
+++ MyNotes/ViewModels/MainScreenViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import SwiftUI
+import RealmSwift
+
+class MainScreenViewModel: ObservableObject{
+    
+    @ObservedRealmObject var notesRowsRealmGroup: NotesRowsRealmGroup
+    @Published var notesCount: Int?
+    
+    init(){
+        let realm = NotesRowsRealmGroup.CreateRealm
+        let noteRowRealmGroupObject = realm.objects(NotesRowsRealmGroup.self)
+        self.notesRowsRealmGroup = noteRowRealmGroupObject.first!
+        self.notesCount = notesRowsRealmGroup.notesRowsRealm.count
+    }
+    
+    func addNote(noteText:String){
+        
+        defer{notesCount = notesRowsRealmGroup.notesRowsRealm.count}
+        
+        let newTask = NoteRowRealm(value: ["id":"\(UUID().uuidString)","noteLabel": "\(noteText)", "noteDescription": ""])
+        
+        $notesRowsRealmGroup.notesRowsRealm.append(newTask)
+        
+        
+        
+    
+    }
+    
+    func deleteNote(_ DeletedNote: NoteRowRealm){
+        
+        defer{notesCount = notesRowsRealmGroup.notesRowsRealm.count}
+        
+//        print(DeletedNote)
+        
+        let realm = try! Realm()
+        try! realm.write {
+            let isDeletedTasks = realm.objects(NoteRowRealm.self).where {
+                $0._id == DeletedNote._id
+            }
+            realm.delete(isDeletedTasks)
+        }
+        
+    
+    }
+    
+    
+    
+}
